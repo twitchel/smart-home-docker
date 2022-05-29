40206805b8e35c948d38ccc46f8227aa2f20bb7f
Daniel K. Jones <me@danielkjones.com>
2022-05-30 09:08:40 +1000

    Fix dockerfile paths

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,11 +4,11 @@ PWD=$(pwd)
 
 source ./scripts/bootstrap.sh "$PWD"
 
-dockerFiles=(-f "$(PWD)/common.docker-compose.yml")
+dockerFiles=(-f "$PWD/common.docker-compose.yml")
 
 if [ "$HOME_ASSISTANT_ENABLED" == "true" ]; then
   if [ -d "$DATADIR/homeassistant" ]; then
-    dockerFiles+=(-f "$(PWD)/apps/homeassistant/docker-compose.yml")
+    dockerFiles+=(-f "$PWD/apps/homeassistant/docker-compose.yml")
   else
     echo "Home assistant config not set. Please run init again."
   fi
@@ -16,7 +16,7 @@ fi
 
 if [ "$ZEROTIER_ENABLED" == "true" ]; then
   if [ -d "$DATADIR/zerotier" ]; then
-    dockerFiles+=(-f "$(PWD)/apps/zerotier/docker-compose.yml")
+    dockerFiles+=(-f "$PWD/apps/zerotier/docker-compose.yml")
   else
     echo "Zerotier config not set. Please run init again."
   fi
