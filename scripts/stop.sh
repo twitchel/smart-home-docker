904980d9fbb2c1cbd6990df175132c1bc68f3a64
Daniel K. Jones <me@danielkjones.com>
2024-04-19 17:30:48 +1000

    Add project name to stop shell script as well (needed to work correctly)

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -8,4 +8,4 @@ if [ -z "$SHD_HOST" ]; then
     read -r -p "SHD_HOST environment variable not defined. Enter the stack name you want to stop: " hostname
     export SHD_HOST=$hostname
 fi
-docker-compose --env-file .env -f docker-compose.yml down
+docker-compose --env-file .env -p "smart-home-docker-$SHD_HOST" -f docker-compose.yml down
